49b6b15d02e5d8eda6abf6ce275606a779dddbca
minzhub <min921029@gmail.com>
2024-05-16 01:26:16 +0900

    Update 가격대 별 상품 개수 구하기.sql

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -3,3 +3,9 @@ SELECT FLOOR(price/10000)*10000 AS price_group, COUNT(*) AS products
 FROM product
 GROUP BY price_group
 ORDER BY price_group
+
+-- 코드를 입력하세요
+SELECT TRUNCATE(price, -LENGTH(price)+1) AS price_group, COUNT(*) AS products
+FROM product
+GROUP BY price_group
+ORDER BY price_group
