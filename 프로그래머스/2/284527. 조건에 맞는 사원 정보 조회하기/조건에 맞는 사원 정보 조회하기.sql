a8c6b51dd74d2eedbc050d8212e75e61165d6bb3
minzhub <min921029@gmail.com>
2024-05-23 01:04:12 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,4 +1,4 @@
--- 코드를 작성해주세요
+-- 가장 평가 점수가 높은 사원의 사번과 점수 추출
 WITH grade AS(
     SELECT emp_no, year, SUM(score) score
     FROM HR_GRADE
@@ -6,5 +6,6 @@ WITH grade AS(
     ORDER BY 3 DESC
     LIMIT 1)
 
+-- 평가 점수가 높은 사원의 사번, 성명, 직책, 이메일 조회
 SELECT grade.score, grade.emp_no, h.emp_name, h.position, h.email
 FROM grade INNER JOIN hr_employees h USING (emp_no)
