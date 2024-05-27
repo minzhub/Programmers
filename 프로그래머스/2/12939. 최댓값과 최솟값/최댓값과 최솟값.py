aeb03881f73bb1c2b7a36e63dd4be065f1195a10
minzhub <min921029@gmail.com>
2024-05-28 02:46:55 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,3 +1,3 @@
 def solution(s):
-    new_s = [int(i) for i in s.split(' ')]
-    return f"{min(new_s)} {max(new_s)}"
+    new_s = list(map(int, s.split(' ')))
+    return str(min(new_s)) + " " + str(max(new_s))
