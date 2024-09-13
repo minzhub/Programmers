49002a1a9f79ef111a38d7089b60bf124a1e26d4
minzhub <min921029@gmail.com>
2024-09-13 23:38:13 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,11 @@
+def solution(price):
+    answer = 0
+    if price < 100000:
+        answer = price
+    elif 100000 <= price < 300000:
+        answer = int(price*0.95)
+    elif 300000 <= price < 500000:
+        answer = int(price*0.9)
+    elif 500000<= price:
+        answer = int(price*0.8)
+    return answer
