9e906c15cab432944a0f4c5989f1a016f326731a
minzhub <min921029@gmail.com>
2024-09-26 01:21:09 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ MySQL/프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,6 +1,5 @@
--- 코드를 입력하세요
-SELECT o.animal_id ANIMAL_ID, o.name NAME
-FROM animal_outs o JOIN animal_ins i
-ON o.animal_id = i.animal_id
+SELECT i.animal_id, i.name
+FROM animal_ins i JOIN animal_outs o
+ON i.animal_id = o.animal_id
 ORDER BY DATEDIFF(o.datetime, i.datetime) DESC
 LIMIT 2;
