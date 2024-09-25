cd147fec19efbcdffc046a1fd23d08b4d72e308d
minzhub <min921029@gmail.com>
2024-09-26 01:28:42 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ MySQL/프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,7 +1,5 @@
--- 코드를 입력하세요
 SELECT i.animal_id, i.animal_type, i.name
-FROM animal_ins i JOIN (SELECT animal_id
-                        FROM animal_outs
-                        WHERE sex_upon_outcome NOT LIKE 'Intact%') o
+FROM animal_ins i JOIN animal_outs o
 ON i.animal_id = o.animal_id
-WHERE sex_upon_intake LIKE 'Intact%'
+WHERE i.sex_upon_intake LIKE '%intact%'
+      AND o.sex_upon_outcome NOT LIKE '%intact%'
