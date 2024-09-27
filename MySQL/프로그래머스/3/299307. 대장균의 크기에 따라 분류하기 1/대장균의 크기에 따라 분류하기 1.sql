7978f5b2a9451cc0e8b5984474d44ee825bccbd4
minzhub <min921029@gmail.com>
2024-09-27 20:24:59 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 1, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/3/299307. 대장균의 크기에 따라 분류하기 1/대장균의 크기에 따라 분류하기 1.sql
@@ -0,0 +1,8 @@
+SELECT id,
+       CASE 
+           WHEN size_of_colony <= 100 THEN 'LOW'
+           WHEN size_of_colony BETWEEN 101 AND 1000 THEN 'MEDIUM'
+           ELSE 'HIGH'
+       END AS size
+FROM ecoli_data
+ORDER BY 1;
