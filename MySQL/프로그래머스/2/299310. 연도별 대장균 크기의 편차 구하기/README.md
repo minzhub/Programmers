a7d219b9287f50da98c2159528a477804480dff4
minzhub <min921029@gmail.com>
2024-10-30 00:11:32 +0900

    [level 2] Title: 연도별 대장균 크기의 편차 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/README.md
+++ MySQL/프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 연도별 대장균 크기의 편차 구하기 - 299310 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/299310) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/299310#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 27일 19:59:53
+2024년 10월 30일 00:11:30
 
 ### 문제 설명
 
--- MySQL/프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/연도별 대장균 크기의 편차 구하기.sql
+++ MySQL/프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/연도별 대장균 크기의 편차 구하기.sql
@@ -1,4 +1,10 @@
-SELECT YEAR(differentiation_date) YEAR,
-       (MAX(size_of_colony) OVER (PARTITION BY YEAR(differentiation_date)) - size_of_colony) YEAR_DEV, id
-FROM ecoli_data
-ORDER BY 1, 2
+WITH max_size AS (
+    SELECT YEAR(differentiation_date) AS year, MAX(size_of_colony) AS mx
+    FROM ecoli_data
+    GROUP BY 1
+)
+
+SELECT m.year, (m.mx - e.size_of_colony) AS year_dev, e.id
+FROM ecoli_data e JOIN max_size m
+ON YEAR(e.differentiation_date) = m.year
+ORDER BY 1, 2;
