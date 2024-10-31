3b7b4daa4b67c2be12fbad6060370cd840e54668
minzhub <min921029@gmail.com>
2024-11-01 00:49:45 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ MySQL/프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,17 +1,12 @@
-WITH month_fee AS (
-    SELECT r.car_id, 
-           c.car_type, 
-           FLOOR(c.daily_fee * 30 * (1 - COALESCE(d.discount_rate, 0) / 100)) AS fee
-    FROM car_rental_company_rental_history r 
-    JOIN car_rental_company_car c ON r.car_id = c.car_id
-    LEFT JOIN car_rental_company_discount_plan d 
-           ON c.car_type = d.car_type AND d.duration_type = '30일 이상'
-    WHERE c.car_type IN ('세단', 'SUV')
-          AND r.start_date < '2022-11-30' 
-          AND r.end_date < '2022-11-01'
-)
-
-SELECT DISTINCT car_id, car_type, fee
-FROM month_fee
-WHERE fee >= 500000 AND fee < 2000000
-ORDER BY fee DESC, car_type ASC, car_id DESC;
+SELECT cc.car_id, cc.car_type, FLOOR(daily_fee * 30 * (1 - discount_rate / 100)) AS fee
+FROM car_rental_company_car cc
+LEFT JOIN car_rental_company_rental_history rh
+ON cc.car_id = rh.car_id AND start_date <= '2022-11-30' AND end_date >= '2022-11-01'
+JOIN car_rental_company_discount_plan dp
+ON cc.car_type = dp.car_type
+AND duration_type = '30일 이상'
+WHERE cc.car_type IN ('세단', 'SUV')
+      AND history_id IS NULL
+GROUP BY 1
+HAVING fee >= 500000 AND fee < 2000000
+ORDER BY 3 DESC, 2, 1 DESC;
