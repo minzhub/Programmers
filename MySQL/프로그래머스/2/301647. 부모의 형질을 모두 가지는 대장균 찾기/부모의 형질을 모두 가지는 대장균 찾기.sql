6d8d41542501a4f43b70895ca9671182214e4bb8
minzhub <min921029@gmail.com>
2024-11-02 17:31:42 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ MySQL/프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -1,5 +1,5 @@
-SELECT ED1.id, ED1.genotype, ED2.genotype AS PARENT_GENOTYPE
-FROM ecoli_data ED1 LEFT JOIN ecoli_data AS ED2
-ON ED1.parent_id = ED2.id
-WHERE ED1.genotype & ED2.genotype = ED2.genotype
-ORDER BY 1
+SELECT e.id, e.genotype, d.genotype AS parent_genotype
+FROM ecoli_data e JOIN ecoli_data d
+ON e.parent_id = d.id
+WHERE e.genotype & d.genotype = d.genotype
+ORDER BY 1;
