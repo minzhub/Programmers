1f49afdba9a0a8b55eb61a246c6314aaff52bcc8
minzhub <min921029@gmail.com>
2024-11-11 22:28:24 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ MySQL/프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,4 @@
 SELECT o.animal_id, o.name
-FROM animal_outs o LEFT JOIN animal_ins i
-ON o.animal_id = i.animal_id
+FROM animal_ins i RIGHT JOIN animal_outs o
+ON i.animal_id = o.animal_id
 WHERE i.animal_id IS NULL
-ORDER BY 1;
