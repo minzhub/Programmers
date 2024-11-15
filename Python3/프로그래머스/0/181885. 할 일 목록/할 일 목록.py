881f434f3a9bca0e8eec4af6ec7e00b243d9dfbb
minzhub <min921029@gmail.com>
2024-11-16 01:16:25 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ Python3/프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,2 @@
 def solution(todo_list, finished):
-    answer = []
-    for a, b in zip(todo_list, finished):
-        if b == False:
-            answer.append(a)
-    return answer
+    return [a for a, b in zip(todo_list, finished) if not b]
