87a653a9dc20c4b11478bd2fd1b5c186a4e7a11c
minzhub <min921029@gmail.com>
2024-12-05 23:36:09 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ MySQL/프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,4 +1,5 @@
+-- 코드를 입력하세요
 SELECT animal_id, name,
-       IF(sex_upon_intake LIKE '%Neutered%' OR sex_upon_intake LIKE'%Spayed%', 'O', 'X') AS '중성화'
+       IF(sex_upon_intake LIKE 'Neutered%' OR sex_upon_intake LIKE 'Spayed%', 'O', 'X')        AS '중성화'
 FROM animal_ins
-ORDER BY 1;
+ORDER BY animal_id;
