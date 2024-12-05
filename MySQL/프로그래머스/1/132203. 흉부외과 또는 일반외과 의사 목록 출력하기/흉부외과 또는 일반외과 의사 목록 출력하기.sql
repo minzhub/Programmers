fc2338b72e571d64add3252727bdd606794c0fef
minzhub <min921029@gmail.com>
2024-12-05 22:03:03 +0900

    [level 1] Title: 흉부외과 또는 일반외과 의사 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ MySQL/프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -1,4 +1,5 @@
+-- 코드를 입력하세요
 SELECT dr_name, dr_id, mcdp_cd, DATE_FORMAT(hire_ymd, '%Y-%m-%d') AS hire_ymd
 FROM doctor
 WHERE mcdp_cd IN ('CS', 'GS')
-ORDER BY 4 DESC, 1 ASC;
+ORDER BY hire_ymd DESC, dr_name;
