63607353fc69765476a7961b686bfe54cf025b86
minzhub <min921029@gmail.com>
2024-12-20 14:13:54 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ MySQL/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,6 +1,8 @@
-SELECT member_id, member_name, gender, DATE_FORMAT(date_of_birth, '%Y-%m-%d') as date_of_birth
+-- 코드를 입력하세요
+SELECT member_id, member_name, gender,
+       DATE_FORMAT(date_of_birth, '%Y-%m-%d') AS date_of_birth
 FROM member_profile
-WHERE MONTH(date_of_birth) = '3'
+WHERE TLNO IS NOT NULL
+      AND MONTH(date_of_birth) = '3'
       AND gender = 'W'
-      AND tlno IS NOT NULL
-ORDER BY 1;
+ORDER BY member_id;
