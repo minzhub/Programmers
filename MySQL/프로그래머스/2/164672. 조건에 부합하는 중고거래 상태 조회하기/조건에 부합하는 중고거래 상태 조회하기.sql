647022f5c457fedb14b3075499826b503229415e
minzhub <min921029@gmail.com>
2024-12-23 22:28:38 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ MySQL/프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,9 +1,10 @@
+-- 코드를 입력하세요
 SELECT board_id, writer_id, title, price,
        CASE status
-           WHEN 'sale' THEN '판매중'
-           WHEN 'reserved' THEN '예약중'
+           WHEN 'SALE' THEN '판매중'
+           WHEN 'RESERVED' THEN '예약중'
            ELSE '거래완료'
        END AS status
 FROM used_goods_board
-WHERE created_date = '2022-10-05'
-ORDER BY 1 DESC;
+WHERE DATE_FORMAT(created_date, '%Y-%m-%d') = '2022-10-05'
+ORDER BY board_id DESC;
