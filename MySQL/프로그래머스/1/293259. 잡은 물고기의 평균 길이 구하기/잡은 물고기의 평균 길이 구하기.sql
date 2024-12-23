0d5d377307ee6e82f4f030304f82dd5fce274ba8
minzhub <min921029@gmail.com>
2024-12-23 23:09:12 +0900

    [level 1] Title: 잡은 물고기의 평균 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
+++ MySQL/프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
@@ -1,2 +1,4 @@
-SELECT ROUND(AVG(IFNULL(length, 10)), 2) AS average_length
-FROM fish_info
+-- 코드를 작성해주세요
+SELECT ROUND(AVG(length), 2) AS average_length
+FROM (SELECT COALESCE(length, 10) AS length
+      FROM fish_info) f;
