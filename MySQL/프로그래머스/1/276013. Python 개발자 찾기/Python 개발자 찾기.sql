8f874ff976cdf2284b5ee446ebf8f46cb86c7b42
minzhub <min921029@gmail.com>
2024-12-23 22:25:15 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ MySQL/프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,7 +1,5 @@
 -- 코드를 작성해주세요
 SELECT id, email, first_name, last_name
 FROM developer_infos
-WHERE skill_1 = 'Python'
-      OR skill_2 = 'Python'
-      OR skill_3 = 'Python'
+WHERE 'Python' IN (skill_1, skill_2, skill_3)
 ORDER BY id;
