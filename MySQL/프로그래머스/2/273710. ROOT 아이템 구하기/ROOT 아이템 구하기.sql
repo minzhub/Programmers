f0f7dacb1db22fc994968dd9d4212f576ff779d1
minzhub <min921029@gmail.com>
2024-12-23 22:13:32 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ MySQL/프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,5 +1,6 @@
-SELECT i.item_id, i.item_name
+-- 코드를 작성해주세요
+SELECT t.item_id, i.item_name
 FROM item_info i JOIN item_tree t
 ON i.item_id = t.item_id
-WHERE parent_item_id IS NULL
-ORDER BY 1;
+WHERE t.parent_item_id IS NULL
+ORDER BY t.item_id;
