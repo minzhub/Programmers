ecc55ebeafb3b078c09c6393d8b1b5ca4768297f
minzhub <min921029@gmail.com>
2024-12-24 15:43:09 +0900

    [level 2] Title: 특정 물고기를 잡은 총 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
+++ MySQL/프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
@@ -1,4 +1,5 @@
-SELECT COUNT(id) AS fish_count
+-- 코드를 작성해주세요
+SELECT COUNT(*) AS fish_count
 FROM fish_info i JOIN fish_name_info n
 ON i.fish_type = n.fish_type
 WHERE n.fish_name IN ('BASS', 'SNAPPER');
