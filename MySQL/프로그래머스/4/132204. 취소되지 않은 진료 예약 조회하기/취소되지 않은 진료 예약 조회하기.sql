348dfabda92a3f461d56c15d2a61720bd01ba9b7
minzhub <min921029@gmail.com>
2024-12-24 15:04:55 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ MySQL/프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,8 +1,9 @@
-SELECT a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
+-- 코드를 입력하세요
+SELECT a.apnt_no, p.pt_name, a.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
 FROM appointment a
 JOIN patient p ON a.pt_no = p.pt_no
-JOIN doctor d ON a.mddr_id = D.dr_id
-WHERE DATE_FORMAT(apnt_ymd, '%Y-%m-%d') = '2022-04-13'
+JOIN doctor d ON a.mddr_id = d.dr_id
+WHERE LEFT(a.apnt_ymd, 10) = '2022-04-13'
       AND a.apnt_cncl_ymd IS NULL
       AND a.mcdp_cd = 'CS'
 ORDER BY a.apnt_ymd;
