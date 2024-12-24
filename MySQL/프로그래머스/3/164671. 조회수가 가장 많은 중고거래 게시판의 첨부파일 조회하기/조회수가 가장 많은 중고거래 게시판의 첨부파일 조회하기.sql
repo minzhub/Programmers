5c0328f0b420ee5da637661f6a4439658f861492
minzhub <min921029@gmail.com>
2024-12-24 16:12:37 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ MySQL/프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,7 +1,12 @@
-SELECT CONCAT('/home/grep/src/', f.board_id, '/', f.file_id, f.file_name, f.file_ext) AS file_path
-FROM used_goods_file f JOIN (SELECT board_id
-                             FROM used_goods_board
-                             ORDER BY views DESC
-                             LIMIT 1) b
-ON f.board_id = b.board_id
-ORDER BY 1 DESC;
+-- 코드를 입력하세요
+WITH hits AS (
+    SELECT board_id
+    FROM used_goods_board
+    ORDER BY views DESC
+    LIMIT 1
+)
+
+SELECT CONCAT('/home/grep/src/', h.board_id, '/', f.file_id, f.file_name, f.file_ext) AS file_path
+FROM hits h JOIN used_goods_file f
+ON h.board_id = f.board_id
+ORDER BY f.file_id DESC;
