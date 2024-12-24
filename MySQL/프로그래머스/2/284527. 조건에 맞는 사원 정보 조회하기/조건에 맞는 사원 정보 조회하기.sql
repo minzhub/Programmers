1d3383a40bc71f64a49dbac1728604150738b8ea
minzhub <min921029@gmail.com>
2024-12-24 15:24:21 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ MySQL/프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,6 +1,12 @@
-SELECT SUM(score) AS score, g.emp_no, e.emp_name, e.position, e.email
-FROM hr_grade g JOIN hr_employees e
-ON g.emp_no = e.emp_no
-GROUP BY 2
-ORDER BY 1 DESC
-LIMIT 1;
+-- 코드를 작성해주세요
+WITH grade AS (
+    SELECT emp_no, SUM(score) AS score
+    FROM hr_grade
+    GROUP BY emp_no
+    ORDER BY score DESC
+    LIMIT 1
+)
+
+SELECT g.score, g.emp_no, e.emp_name, e.position, e.email
+FROM grade g JOIN hr_employees e
+ON g.emp_no = e.emp_no;
