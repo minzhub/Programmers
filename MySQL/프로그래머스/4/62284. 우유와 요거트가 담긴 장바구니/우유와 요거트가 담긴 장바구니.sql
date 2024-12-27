3e5396e14c62c4713da297cb20bead0df634dbd8
minzhub <min921029@gmail.com>
2024-12-27 20:44:37 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,6 +1,8 @@
-SELECT CART_ID
-FROM CART_PRODUCTS
-WHERE NAME IN ('Milk', 'Yogurt')
-GROUP BY CART_ID
-HAVING COUNT(DISTINCT NAME) = 2
-ORDER BY CART_ID;
+-- 코드를 입력하세요
+SELECT cart_id
+FROM (SELECT cart_id, GROUP_CONCAT(name) AS names
+      FROM cart_products
+      GROUP BY cart_id) c
+WHERE names LIKE '%Milk%'
+      AND names LIKE '%Yogurt%'
+ORDER BY cart_id;
