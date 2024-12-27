99e8347413caa92f3f63d315cb58261b6090618b
minzhub <min921029@gmail.com>
2024-12-27 20:56:17 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ MySQL/프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,7 +1,8 @@
+-- 코드를 입력하세요
 SELECT *
 FROM places
 WHERE host_id IN (SELECT host_id
                   FROM places
                   GROUP BY host_id
-                  HAVING COUNT(host_id) >= 2)
+                  HAVING COUNT(name) >= 2)
 ORDER BY id;
