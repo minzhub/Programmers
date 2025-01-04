581909abad295adf9b32ea921ddb7340b40cf402
minzhub <min921029@gmail.com>
2025-01-04 19:13:20 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
+++ MySQL/프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -1,5 +1,12 @@
-SELECT i.item_id, i.item_name, i.rarity
-FROM item_info i LEFT JOIN item_tree t
-ON i.item_id = t.parent_item_id
-WHERE t.item_id IS NULL
-ORDER BY 1 DESC;
+-- 코드를 작성해주세요
+WITH ug AS (
+    SELECT i1.item_id
+    FROM item_tree i1 LEFT JOIN item_tree i2
+    ON i1.item_id = i2.parent_item_id
+    WHERE i2.item_id IS NULL
+)
+
+SELECT u.item_id, i.item_name, i.rarity
+FROM ug u JOIN item_info i
+ON u.item_id = i.item_id
+ORDER BY u.item_id DESC;
