f34e50f00079b9c1595cd05c14c543186978535b
minzhub <min921029@gmail.com>
2025-02-10 16:23:40 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ MySQL/프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,13 +1,14 @@
-WITH RECURSIVE hours AS (
-    SELECT 0 AS hour
+-- 코드를 입력하세요
+WITH RECURSIVE time AS (
+    SELECT 0 hour
     UNION ALL
-    SELECT hour + 1
-    FROM hours
+    SELECT hour+1 AS hour
+    FROM time
     WHERE hour < 23
 )
 
-SELECT h.hour, COUNT(a.animal_id) AS count
-FROM hours h LEFT JOIN animal_outs a
-ON h.hour = HOUR(a.datetime)
-GROUP BY h.hour
-ORDER BY h.hour;
+SELECT t.hour, COUNT(a.animal_id) AS count
+FROM time t LEFT JOIN animal_outs a
+ON t.hour = HOUR(a.datetime)
+GROUP BY t.hour
+ORDER BY t.hour;
